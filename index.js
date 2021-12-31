40298b67c28bf9afd72cfe61e8edb1410ea6ce25
ysnbthn <yasinbatuhanozyurek@gmail.com>
2021-12-31 16:05:53 +0300

    add prototype function for shooting guys

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const shootingArea = document.querySelector('.shootingArea');
 const aim = document.querySelector('.aim');
-
-
+const badGuys = document.getElementsByClassName('badGuy');
 const saCoordinates = shootingArea.getBoundingClientRect();
+
+var score = 0;
+
+//console.log(badGuys);
+
+for (var i = 0; i<badGuys.length; i++) {
+    badGuys[i].addEventListener('click', clickCallBack);
+}
+
+function clickCallBack(e) {
+
+    var targetArea = e.target.getBoundingClientRect();
+
+    console.log(targetArea);
+    console.log(e.clientX, e.clientY);
+
+    console.log(e.target.id);
+}
+
+
+
+
 //console.log(saCoordinates.top, saCoordinates.left, saCoordinates.right, saCoordinates.bottom);
 
 
@@ -10,7 +31,7 @@ const saCoordinates = shootingArea.getBoundingClientRect();
 // aim movement
 shootingArea.addEventListener('mousemove', function(e){
     
-    console.log(e); // debug
+    //console.log(e); // debug
 
     // because aim size (50 * 50) px
     if(e.pageY +50 >= saCoordinates.top && e.pageY +50 <= saCoordinates.bottom && e.pageX +50 >= saCoordinates.left && e.pageX +50 <= saCoordinates.right){
@@ -29,11 +50,15 @@ aim.addEventListener('click', e=> {
     var clickedTarget = e.target;
     var targetCoordinates = clickedTarget.getBoundingClientRect();
     
+    console.log(e.target);
     
-    
-    
-    document.querySelector('#dbt2').textContent = `Click X: ${e.clientX}, Click Y: ${e.clientY}`; 
+    document.querySelector('#dbt2').textContent = `Click X: ${e.clientX}, Click Y: ${e.clientY}`;
 });
+
+function addScore(scr){
+    scr += 100;
+    document.querySelector('.scoreText').innerText = `Score: ${scr}`;
+}
 
     
 

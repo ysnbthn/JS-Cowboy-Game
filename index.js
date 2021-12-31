30690ae6d47545d69e7d20767dfe04a1606c7713
ysnbthn <yasinbatuhanozyurek@gmail.com>
2021-12-31 14:50:55 +0300

    add bad guys

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const shootingArea = document.querySelector('.shootingArea');
 const aim = document.querySelector('.aim');
 
+
+const saCoordinates = shootingArea.getBoundingClientRect();
+//console.log(saCoordinates.top, saCoordinates.left, saCoordinates.right, saCoordinates.bottom);
+
+
+
+// aim movement
 shootingArea.addEventListener('mousemove', function(e){
     
-    console.log(e);
-    
-    aim.style.left = e.clientX + 'px';
-    aim.style.top = e.clientY + 'px';
+    console.log(e); // debug
 
-    // For debug
-    document.querySelector('#dbt1').textContent = `Aim X: ${e.clientX}, Aim Y: ${e.clientY}`; 
+    // because aim size (50 * 50) px
+    if(e.pageY +50 >= saCoordinates.top && e.pageY +50 <= saCoordinates.bottom && e.pageX +50 >= saCoordinates.left && e.pageX +50 <= saCoordinates.right){
+        aim.style.left = e.clientX + 'px';
+        aim.style.top = e.clientY + 'px';
+
+
+        // For debug
+        document.querySelector('#dbt1').textContent = `Aim X: ${e.clientX}, Aim Y: ${e.clientY}`; 
+    }
 });
+
+aim.addEventListener('click', e=> {
+    
+    const aimCoordinates = aim.getBoundingClientRect();
+    
+
+});
+
+    
+
+
+
+
